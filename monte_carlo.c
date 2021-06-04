43674ba95d1c397fac851fdd813f27436d8529d9
KadeejathilKubra <84235107+KadeejathilKubra@users.noreply.github.com>
2021-06-04 17:31:01 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -42,7 +42,7 @@ float mc_pi(int n){
 	int a=0;
 	int b =0;
 	float pi;  
-	for (i = 0; i < n ; i++){
+	for (int i = 0; i < n ; i++){
 	    float x = frandom();                         
    	    float y = frandom();                                                
    	   float distance = (x*x) + (y*y);
